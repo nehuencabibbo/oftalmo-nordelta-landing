5ae888d3399e57085b9bec906af34599be2e00c9
Nehuén Cabibbo <cabibbo.nehuen@gmail.com>
2025-10-29 01:05:40 -0300

    fix: Changing hero description

--- src/pages/sections/Hero.tsx
+++ src/pages/sections/Hero.tsx
@@ -18,7 +18,7 @@ export default function Hero() {
       <div className="z-10 text-center px-4 max-w-4xl mx-auto flex flex-col gap-5">
         <h1 className="text-shadow-lg/20 text-8xl font-semibold">Oftalmo Nordelta</h1>
         <p className="text-shadow-lg text-md tracking-wide px-35">
-          Cuidando la visión de nuestros pacientes hace más de <span className="font-bold text-accent">7 años</span>. Junto a profesionales con más de <span className="font-bold text-accent">20 años</span> de experiencia. 
+          Cuidando la visión de nuestros pacientes hace más de <span className="font-bold text-accent">7 años</span>, respaldados por profesionales con más de <span className="font-bold text-accent">20 años</span> de experiencia en el sector. 
         </p>
       </div>
     </section>
