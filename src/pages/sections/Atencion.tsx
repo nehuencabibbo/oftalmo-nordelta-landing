a04f890c385c3d3dfcfd15d70ef60c7688bcf683
Nehuén Cabibbo <cabibbo.nehuen@gmail.com>
2025-11-09 22:25:56 -0300

    feat(atencion): Responsivness fixes

--- src/pages/sections/Atencion.tsx
+++ src/pages/sections/Atencion.tsx
@@ -1,23 +1,35 @@
 const treatments = [
   {
-    title: "Campo Visual Computado",
+    title: "Campo\u00A0Visual Computado",
     description:
-      "Es una prueba que evalúa el alcance de la visión periférica de una persona, logrando detección precoz y evolución comparada de variadas lesiones oftalmológicas y neurológicas. Contamos con el moderno sistema suizo de diagnóstico computarizado Octopus con la Perimetría Pulsar para la detección temprana del Glaucoma.",
-  },
-  {
-    title: "Tomografía de Coherencia óptica (OCT)",
-    description:
-      "Estudio de imagen no invasivo y de no contacto que permite evaluar con alta resolución las estructuras internas de la retina y el nervio óptico in vivo. Aplicaciones clínicas: Detección precoz de enfermedades de la retina como las Maculopatías, además permite un monitoreo de la enfermedad y su respuesta al tratamiento.",
-  },
-  {
-    title: "Tomógrafo de nervio óptico (HRT)",
-    description:
-      "Es un dispositivo de tomografía láser confocal diseñado para generar imágenes tridimensionales de la cabeza del nervio óptico y la capa de fibras nerviosas. Aplicaciones clínicas: Diagnóstico y seguimiento del glaucoma; screening en población de alto riesgo; detección de progresión glaucomatosa, etc.",
+      "Es una prueba que evalúa el alcance de la visión periférica de una persona, logrando detección precoz y evolución comparada de variadas lesiones oftalmológicas y neurológicas.",
+    detail:
+      "Contamos con el moderno sistema suizo de diagnóstico computarizado Octopus con la Perimetría Pulsar para la detección temprana del Glaucoma.",
+    clincApplications: false,
   },
   {
     title: "Topografía Corneal",
     description:
-      "La topografía corneal es un mapeo computado de la superficie de la córnea no invasivo. Permite analizar la forma y curvatura de la córnea, ayudando a diagnosticar enfermedades y planificar tratamientos o cirugías oculares. Aplicaciones clínicas: es muy útil en el diagnóstico y seguimiento de enfermedades corneales, como el queratocono, la degeneración marginal pelúcida, los astigmatismos irregulares, etc.",
+      "Es un mapeo computado de la superficie de la córnea no invasivo. Permite analizar la forma y curvatura de la córnea, ayudando a diagnosticar enfermedades y planificar tratamientos o cirugías oculares.",
+    detail:
+      "Es muy útil en el diagnóstico y seguimiento de enfermedades corneales, como el queratocono, la degeneración marginal pelúcida, los astigmatismos irregulares, etc.",
+    clincApplications: true,
+  },
+  {
+    title: "Tomografía\u00A0de Coherencia\u00A0Óptica (OCT)",
+    description:
+      "Estudio de imagen no invasivo y de no contacto que permite evaluar con alta resolución las estructuras internas de la retina y el nervio óptico in vivo.",
+    detail:
+      "Detección precoz de enfermedades de la retina como las Maculopatías, además permite un monitoreo de la enfermedad y su respuesta al tratamiento.",
+    clincApplications: true,
+  },
+  {
+    title: "Tomógrafo\u00A0de Nervio\u00A0Óptico (HRT)",
+    description:
+      "Es un dispositivo de tomografía láser confocal diseñado para generar imágenes tridimensionales de la cabeza del nervio óptico y la capa de fibras nerviosas.",
+    detail:
+      "Diagnóstico y seguimiento del glaucoma; screening en población de alto riesgo; detección de progresión glaucomatosa, etc.",
+    clincApplications: true,
   },
 ];
 
@@ -25,16 +37,16 @@ export default function Atencion() {
   return (
     <section
       id="atencion"
-      className="flex flex-col items-center w-full h-screen bg-secondary"
+      className="flex flex-col items-center w-full py-15 bg-secondary"
     >
       <h2>Tratamientos</h2>
-      <div className="grid grid-cols-1 md:grid-cols-2 gap-12 w-full text-shadow-lg px-20">
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-12 w-full text-shadow-lg px-12 md:px-20">
         {treatments.map((treatment) => (
           <div
             key={treatment.title}
             className="flex flex-col gap-8 items-center text-center"
           >
-            <div className="flex flex-col w-full gap-4">
+            <div className="flex flex-col gap-4 w-full">
               <h3 className="text-3xl font-bold">{treatment.title}</h3>
               {/* Separator */}
               <div className="flex items-center justify-center gap-3 px-2">
@@ -43,7 +55,20 @@ export default function Atencion() {
                 <span className="h-px flex-1 bg-custom-white/60" />
               </div>
             </div>
-            <p className="text-justify text-lg">{treatment.description}</p>
+            <p className="text-justify text-lg h-[45%] md:h-[180px] lg:h-[130px] xl:h-[85px]">
+              {treatment.description}
+            </p>
+            <p className="text-justify text-base text-custom-white/80">
+              {treatment.clincApplications ? (
+                <span className="inline-block underline">
+                  Aplicaciones clínicas:
+                </span>
+              ) : (
+                <></>
+              )}
+              {treatment.clincApplications ? " " : <></>}
+              {treatment.detail}
+            </p>
           </div>
         ))}
       </div>
