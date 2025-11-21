da938a64604e477f2c0477a88ed57a89b1218006
nehuencabibbo <86394807+nehuencabibbo@users.noreply.github.com>
2025-11-21 21:16:31 +0000

    Build: (c8f62b9) chore(workflows): Add permissions to GitHub Actions workflow for build and push

--- src/utils/contact.ts
+++
@@ -1,5 +0,0 @@
-import { PhoneNumber, WhatsappLink } from "../constants";
-
-export const getWhatsappContactLinkWithMessage = (message: string) => {
-  return `${WhatsappLink}/${PhoneNumber}?text=${encodeURIComponent(message)}`;
-};
